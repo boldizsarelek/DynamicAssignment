4e60ac8d08617d726157600210b931396d1b42f7
boldizsarelek <boldizsar.elek@stud.uni-corvinus.hu>
2022-09-28 13:07:35 +0200

    Readding files

---
+++ OR-test/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace OR_test
+{
+    public class Application
+    {
+        public Application()
+        {
+        }
+        public int ApplicationID;
+        public int StudentRank;
+        public int CompanyRank;
+        public int CompanyRank2;
+        public int ApplicationID2;
+        public int StudentID;
+        public int CompanyID;
+        public bool Attribute1;
+    }
+}
+
---
+++ OR-test/Constraint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace OR_test
+{
+	public class Constraint
+	{
+		public Constraint()
+		{
+		}
+		public int ConstraintID;
+		public char Type;
+		public int CompanyID;
+		public int MinLimit;
+		public int MaxLimit;
+		public int Szp_ID;
+	}
+}
+
